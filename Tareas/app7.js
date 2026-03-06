80c80307ab218e9ba1f68582882188bc2cec458e
JoshMisael <joshuabautista321@aragon.unam.mx>
2026-03-06 07:52:26 -0600

    Add files via upload

--- Tareas/app7.js
+++ Tareas/app7.js
@@ -1,24 +1,37 @@
-let opcionUser = prompt(`
-    Elija una opcion:
-    1: Semana
-    2: Peliculas
-    3: Juegos
-    4: Dias de las semanas
+let opci = prompt(`
+    Elija un dia de la semana:
+    1. Lunes
+    2. Martes
+    3. Miercoles
+    4. Jueves
+    5. Viernes
+    6. Sabado 
+    7. Viernes
     `);
 
-switch (opcionUser){
+switch(opci){
     case "1":
-        console.log("1984");
+        console.log("Has elegido la opcion Lunes");
         break;
     case "2":
-        console.log("Matrix");
+        console.log("Has elegido la opcion Martes");
         break;
     case "3":
-        console.log("Minecraft");
+        console.log("Has elegido la opcion Miercoles");
         break;
     case "4":
-        console.log("Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo");
+        console.log("Has elegido la opcion Jueves");
+        break;
+    case "5":
+        console.log("Has elegido la opcion Viernes");
+        break;                 
+    case "6":
+        console.log("Has elegido la opcion Sabado");
+        break;
+    case "7":
+        console.log("Has elegido la opcion Domingo");
+        break;             
     default:
-        console.log("No valido");
-
+        console.log("Opcion no valida");
+        break;
 }
