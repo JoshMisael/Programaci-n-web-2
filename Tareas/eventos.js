186b6ed3099cd169b452ac3fe188b4ebc93cbd15
JoshMisael <joshuabautista321@aragon.unam.mx>
2026-04-13 21:58:33 -0600

    Add files via upload

---
+++ Tareas/eventos.js
@@ -0,0 +1,13 @@
+let titulo1 = document.querySelector("#tituloID1");
+let titulo2 = document.querySelector("#tituloID2");
+let titulo3 = document.querySelector("#tituloID3");
+
+console.log(titulo1.textContent)
+console.log(titulo2.textContent)
+console.log(titulo3.textContent)
+
+titulo1.textContent = "Nuevo titulo 1";
+titulo2.textContent = "Nuevo titulo 2";
+titulo3.textContent = "Nuevo titulo 3";
+
+const h1 = document.querySelector('#titulo.')
